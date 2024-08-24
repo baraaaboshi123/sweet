d0d41454e0621813f1cb475442c721e8d5c55a71
baraa aboshi <91554496+baraaaboshi123@users.noreply.github.com>
2024-08-24 14:21:37 +0300

    Update AcceptanceTest.java

--- src/test/java/acceptance/AcceptanceTest.java
+++ src/test/java/acceptance/AcceptanceTest.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        features = "Features/Admin",
+        features = "Features/owner_supplier/CommunicationAndNotification.feature",
         plugin = {"html:target/cucumber-report/report.html"},
         monochrome = true,
         glue = "acceptance"
